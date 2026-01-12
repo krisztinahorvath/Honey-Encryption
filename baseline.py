2f574c3de4ccbfa4cd469180fc49a931eb858824
krisztinahorvath <91715582+krisztinahorvath@users.noreply.github.com>
2026-01-12 21:57:57 +0200

    Add file structure

---
+++ baseline.py
@@ -0,0 +1,22 @@
+"""
+Traditional password-based encryption to compare with Honey Encryption
+"""
+
+
+# TODO:
+# [ ] Implement simple password-based encryption
+# [ ] Wrong password should clearly fail
+# [ ] Keep logic simple
+
+def encrypt(message, password):
+    """
+    Encrypt a message with a password
+    """
+    raise NotImplementedError
+
+def decrypt(ciphertext, password):
+    """
+    Decrypt ciphertext with a password
+    Raise an error or return None on wrong password
+    """
+    raise NotImplementedError
